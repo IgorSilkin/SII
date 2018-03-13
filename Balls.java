8497eea51e022be7c10ae7065c9cec27a450fd07
IgorSilkin <36985981+IgorSilkin@users.noreply.github.com>
2018-03-13 12:12:22 +0300

    Как-то работает

--- Balls.java
+++ Balls.java
@@ -2,37 +2,29 @@
 import java.util.Random;
 
 public class Balls {
-    int row = 3;
-    int column = 4;
-    int oper = column + row;
-    int Depth = 5;
-    int scetchik = 0;
-    int depthArraySearching[][][][] = new int [Depth][oper][row][column];
 
-    int maxBallsArray[] = new int[column];
-    int array[][] = new int[row][column];
-    final int cloneArray[][] = new int[row][column];
+    int ROW = 3;
+    int COLUMN = 4;
+    int maxBallsArray [] = new int[COLUMN];
+    int array [][] = new int[ROW][COLUMN];
 
-    boolean finish = false;
-    boolean failed = false;
     final Random rand = new Random();
 
     public void addBalls(){
         int k;
         boolean ok;
 
-        for(int i = 0; i < column; i++) {
-            maxBallsArray[i] = row;
+        for(int i = 0; i < COLUMN; i++) {
+            maxBallsArray[i] = ROW;
         }
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < column; j++){
+        for(int i = 0; i < ROW; i++){
+            for(int j = 0; j < COLUMN; j++){
                 ok = false;
                 while (ok == false){
-                    k = rand.nextInt(column);
+                    k = rand.nextInt(COLUMN);
                     if(maxBallsArray[k] > 0){
                         maxBallsArray[k]--;
                         array[i][j] = k;
-                        cloneArray[i][j] = k;
                         ok = true;
                     }
                 }
@@ -40,128 +32,43 @@ public class Balls {
         }
     }
 
-    public void moveUp(int col, boolean flagHardMode, int level, int operation){
-        if(flagHardMode == false){
-            int k = array[0][col];
-            for(int i = 0; i < row - 1; i++){
-                array[i][col] = array[i+1][col];
-            }
-            array[row-1][col] = k;
-        }else {
-            moveUp(col, false, 0, 0);
-            depthArraySearching[level][operation] = array.clone();
+    public void moveUp(int col){
+        int k = array[0][col];
+        for(int i = 0; i < ROW - 1; i++){
+            array[i][col] = array[i+1][col];
         }
+        array[ROW-1][col] = k;
     }
-    public void moveLeft(int row, boolean flagHardMode, int level, int operation){
-        if(flagHardMode == false) {
-            int k = array[row][0];
-            for (int i = 0; i < column - 1; i++) {
-                array[row][i] = array[row][i + 1];
-            }
-            array[row][column - 1] = k;
-        }else {
-            moveLeft(row, false, 0, 0);
-            depthArraySearching[level][operation] = array.clone();
+    public void moveLeft(int row){
+        int k = array[row][0];
+        for (int i = 0; i < COLUMN - 1; i++) {
+            array[row][i] = array[row][i + 1];
         }
+        array[row][COLUMN - 1] = k;
     }
 
-    public void show(){
+    public int [][] show(int [][] array, int row, int col){
         System.out.println();
         for(int i = 0; i < row; i++){
-            for(int j = 0; j < column; j++){
+            for(int j = 0; j < col; j++){
                 System.out.print(array[i][j] + "\t");
             }
             System.out.println();
         }
+        return array;
     }
-    public void showClone(){
-        System.out.println();
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < column; j++){
-                System.out.print(cloneArray[i][j] + "\t");
+    public String goToString(){
+        String str = "";
+        for(int i = 0; i < ROW; i++){
+            for(int j = 0; j < COLUMN; j++){
+                str += array[i][j];
             }
-            System.out.println();
         }
-    }
-    public void search(){
-        while (finish == false){
-            //easyMode();
-            hardMode();
-        }
-        if(finish == true && failed == true){
-            System.out.println("Решение не найдено!");
-        }
-        else{
-            System.out.println("------------------------------------------");
-            show();
-            System.out.println("\nОперация: " + scetchik);
-            System.out.println("------------------------------------------");
-        }
-    }
-    public void easyMode(){
-        int k = rand.nextInt(2);
-        if(k == 0){
-            moveUp(rand.nextInt(column), false, 0, 0);
-        }else {
-            moveLeft(rand.nextInt(row), false, 0, 0);
-        }
-        finish = finish();
+        return str;
     }
 
-    public void hardMode(){
-        for(int lvl = 0; lvl < Depth; lvl++){
-            for(int operation = 0; operation < oper; operation++){
-                if(lvl == 0){
-                    Clone();
-                    if(operation < column){
-                        moveUp(operation, true, lvl, operation);
-                    }else if(operation < oper){
-                        moveLeft(operation - column, true, lvl, operation);
-                    }
-                }else if(lvl > 0){
-                    //array = (depthArraySearching[lvl-1][operation]).clone();
-                    bigClone(lvl-1, operation);
-                    if(operation < column){
-                        moveUp(operation, true, lvl, operation);
-                    }else if(operation < oper){
-                        moveLeft(operation - column, true, lvl, operation);
-                    }
-                }
-            }
-        }
-        for (int lvl = Depth-1; lvl >= 0; lvl--){
-            for (int operation = 0; operation < oper; operation++){
-                array = depthArraySearching[lvl][operation].clone();
-                finish = finish();
-                if(lvl == 0 && operation == oper - 1 && finish == false){
-                    /*for(int t = 0; t < Depth; t++){
-                        array = depthArraySearching[t][0].clone();
-                        show();
-                        System.out.println("----"+ t + "----");
-                    }*/
 
-                }finish = true;
-                failed = true;
-            }
-        }
-    }
-
-    public void Clone(){
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < column; j++){
-                array[i][j] = cloneArray[i][j];
-            }
-        }
-    }
-    public void bigClone(int level, int operation){
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < column; j++){
-                array[i][j] = depthArraySearching[level][operation][i][j];
-            }
-        }
-    }
     public boolean finish(){
-        scetchik++;
         if(
             array[0][0] == array[1][0] &&
             array[1][0] == array[2][0] &&
--- Main.java
+++ Main.java
@@ -1,8 +1,57 @@
+import java.util.ArrayList;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        Balls game = new Balls();
-        game.addBalls();
-        game.show();
-        game.search();
+        final Random rand = new Random();
+        boolean finish, fail = false, search;
+        int k = 1, s = 0, checks = 1, checksTrue = 0;
+        ArrayList<String> visited_tops = new ArrayList();
+        Balls currentMap = new Balls();
+        currentMap.addBalls();
+        finish = currentMap.finish();
+        visited_tops.add(0, "" + currentMap.goToString());
+        currentMap.show(currentMap.array, currentMap.ROW, currentMap.COLUMN);
+        while(finish == false && fail == false){
+            s++;
+            if(s >= 7) {
+                s = s - 7;
+            }
+            search = false;
+            if(visited_tops.contains(currentMap.goToString())){
+                search = true;
+                checksTrue++;
+            }
+            if (search == false){
+                visited_tops.add(k, "" + currentMap.goToString());
+                k++;
+                checks++;
+                finish = currentMap.finish();
+            }
+            if(finish != true){
+                if(s < currentMap.ROW){
+                    currentMap.moveLeft(rand.nextInt(currentMap.ROW));
+                }else{
+                    currentMap.moveUp(rand.nextInt(currentMap.COLUMN));
+                }
+            }
+            /*if (rand.nextBoolean() == true){
+                currentMap.moveLeft(rand.nextInt(currentMap.ROW));
+            }else {
+                currentMap.moveUp(rand.nextInt(currentMap.COLUMN));
+            }*/
+            if(checks == 100000 || checksTrue == 5000){
+                fail = true;
+            }
+        }
+        if(finish == true) {
+            System.out.print("\nЧисло проверок:" + checks);
+            System.out.print("\nЧисло отброшенных проверок:" + checksTrue);
+            currentMap.show(currentMap.array, currentMap.ROW, currentMap.COLUMN);
+        }
+        if(fail == true) {
+            System.out.print("\nЧисло проверок достигло максимума:" + checks);
+            System.out.print("\nЧисло отброшенных проверок:" + checksTrue);
+            System.out.print("\nРешение не найдено!");
+        }
     }
 }
